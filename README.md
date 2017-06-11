e9e3d5402f248ad7c7663806f727b0f9ad86426e
Ranc58 <rancvova@gmail.com>
2017-06-11 22:25:13 +0300

    init README.md refactor create_site.py for more funcs edit templates for correct display some symbols

--- README.md
+++ README.md
@@ -1,6 +1,38 @@
 # Encyclopedia
 
-[TODO. There will be project description]
+This program create html site with DevMan wiki about Python by markdown articles and json config file, where stored structure of site.
+
+# How to install
+1. Recomended use venv or virtualenv for better isolation.\
+   Venv setup example: \
+   `python3 -m venv myenv`\
+   `source myenv/bin/activate`
+2. Install requirements: \
+   `pip3 install -r requirements.txt` (alternatively try add `sudo` before command)
+
+# How to use
+If it is needed edit `config.json` and put .md files to dir `site` in necessary folder.\
+Then run program: `python3 create_site.py`.
+
+Structure of site for example:
+```
+├── index.html
+├── site
+│   ├── css
+│   │   ├── blog.css
+│   │   ├── bootstrap.css
+│   ├── 0_tutorial
+│   │   ├──7_codenvy.html
+│   │   ├──8_cli.html
+│   │   ├──14_google.html
+│   ├── 1_python_basics
+│   │   ├──1_intro.html
+│   │   ├──2_base_types.html
+│   ├── 2_html
+│   │   ├──html_injection.html
+│   │   ├──special & symbol.html
+```
+
 
 # Project Goals
 
--- create_site.py
+++ create_site.py
@@ -1,9 +1,13 @@
-
 import os
 import shutil
 import json
 from jinja2 import FileSystemLoader, Environment
 import markdown
+
+
+INDEX_TEMPLATE_HTML = 'index.html'
+ARTICLE_TEMPLATE_HTML = 'article.html'
+JSON_FILE = 'config.json'
 
 
 def load_json_content(json_file):
@@ -23,58 +27,64 @@ def create_site_structure(structure):
             os.mkdir('site/{}'.format(adrticle_dir_name))
 
 
-def create_main_page(structure):
+def create_templates():
     loader = FileSystemLoader('templates',
                               followlinks=True,
                               encoding='utf-8')
     env = Environment(loader=loader, trim_blocks=True,
                       lstrip_blocks=True)
-    template = env.get_template('index.html')
+    index_template = env.get_template(INDEX_TEMPLATE_HTML)
+    article_template = env.get_template(ARTICLE_TEMPLATE_HTML)
+    return index_template, article_template
+
+
+def get_index_page_content(structure):
     topics = structure['topics']
     articles = structure['articles']
-    data = {'links': topics, 'articles': articles}
+    content = {'links': topics, 'articles': articles}
+    return content
+
+
+def create_index_page(template, content):
     with open("index.html", "w") as f:
-        f.write(template.render(data))
+        f.write(template.render(content))
 
 
-"""
-def get_html_from_md(structure):
-    articles = structure['articles']
-    for article in articles:
-        input_file = open('articles/{}'.format(article['source']),
-                          mode="r",
-                          encoding="utf-8")
-        text = input_file.read()
-        html = markdown.markdown(text)
-        return html
-"""
+def get_article_md_content(article):
+    with open('articles/{}'.format(article['source']),
+              mode="r",
+              encoding="utf-8") as md_article:
+        md_content = md_article.read()
+        return md_content
 
 
-def create_articles(structure):
-    loader = FileSystemLoader('templates',
-                              followlinks=True
-                              )
-    env = Environment(loader=loader, trim_blocks=True,
-                      lstrip_blocks=True)
-    template = env.get_template('article.html')
-    articles = structure['articles']
-    for article in articles:
-        input_file = open('articles/{}'.format(article['source']),
-                          mode="r",
-                          encoding="utf-8")
-        text = input_file.read()
-        title = article['title']
-        content = markdown.markdown(text)
-        data = {'title': title, 'content': content}
-        root, ext = os.path.splitext(article['source'])
-        with open('site/{}.html'.format(root), "w") as f:
-            f.write(template.render(data))
+def get_article_page_content(md_content):
+    content = markdown.markdown(md_content)
+    title = article['title']
+    content = {'title': title, 'content': content}
+    root, ext = os.path.splitext(article['source'])
+    return content, root
+
+
+def create_article_page(template, pahtway, content):
+    with open('site/{}.html'.format(pahtway), "w") as f:
+        f.write(template.render(content))
 
 
 if __name__ == '__main__':
-    json_file = 'config.json'
-    structure = load_json_content(json_file)
-    create_site_structure(structure)
-    create_main_page(structure)
-    create_articles(structure)
-
+    try:
+        structure = load_json_content(JSON_FILE)
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        print('Error! Check that json file is exist and correct!')
+    else:
+        create_site_structure(structure)
+        index_content = get_index_page_content(structure)
+        index_template = create_templates()[0]
+        article_template = create_templates()[1]
+        create_index_page(index_template, index_content)
+        articles = structure['articles']
+        for article in articles:
+            md_content = get_article_md_content(article)
+            article_content = get_article_page_content(md_content)[0]
+            pahtway = get_article_page_content(md_content)[1]
+            create_article_page(article_template, pahtway, article_content)
