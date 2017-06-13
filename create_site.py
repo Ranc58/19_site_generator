3ffe80929ed0043fc289c3901d6a157da9ad5a4c
Ranc58 <rancvova@gmail.com>
2017-06-13 11:26:50 +0300

    Edit html templates for custom ways to css. Edit create_site.py for right var names and custom ways to css in templates.

--- create_site.py
+++ create_site.py
@@ -4,10 +4,11 @@ import json
 from jinja2 import FileSystemLoader, Environment
 import markdown
 
-
 INDEX_TEMPLATE_HTML = 'index.html'
 ARTICLE_TEMPLATE_HTML = 'article.html'
 JSON_FILE = 'config.json'
+INDEX_CSS_PATHWAY = './site/css'
+ARTICLE_CSS_PATHWAY = '../css'
 
 
 def load_json_content(json_file):
@@ -20,8 +21,8 @@ def create_site_structure(structure):
         os.mkdir('site')
     if not os.path.exists('site/css'):
         shutil.copytree('templates/css', 'site/css')
-    for dir in structure['articles']:
-        article = dir['source']
+    for pathfolder in structure['articles']:
+        article = pathfolder['source']
         adrticle_dir_name = os.path.split(article)[0]
         if not os.path.exists('site/{}'.format(adrticle_dir_name)):
             os.mkdir('site/{}'.format(adrticle_dir_name))
@@ -38,10 +39,12 @@ def create_templates():
     return index_template, article_template
 
 
-def get_index_page_content(structure):
+def get_index_page_content(structure, INDEX_CSS_PATHWAY):
     topics = structure['topics']
     articles = structure['articles']
-    content = {'links': topics, 'articles': articles}
+    content = {'links': topics,
+               'articles': articles,
+               'css_pathfolder': INDEX_CSS_PATHWAY}
     return content
 
 
@@ -58,15 +61,17 @@ def get_article_md_content(article):
         return md_content
 
 
-def get_article_page_content(md_content):
+def get_article_content(md_content, ARTICLE_CSS_PATHWAY):
     content = markdown.markdown(md_content)
     title = article['title']
-    content = {'title': title, 'content': content}
+    content = {'title': title,
+               'content': content,
+               'article_css_folder': ARTICLE_CSS_PATHWAY}
     root, ext = os.path.splitext(article['source'])
     return content, root
 
 
-def create_article_page(template, pahtway, content):
+def create_article_page(template, content, pahtway):
     with open('site/{}.html'.format(pahtway), "w") as f:
         f.write(template.render(content))
 
@@ -78,13 +83,12 @@ if __name__ == '__main__':
         print('Error! Check that json file is exist and correct!')
     else:
         create_site_structure(structure)
-        index_content = get_index_page_content(structure)
-        index_template = create_templates()[0]
-        article_template = create_templates()[1]
+        index_content = get_index_page_content(structure, INDEX_CSS_PATHWAY)
+        index_template, article_template = create_templates()
         create_index_page(index_template, index_content)
         articles = structure['articles']
         for article in articles:
             md_content = get_article_md_content(article)
-            article_content = get_article_page_content(md_content)[0]
-            pahtway = get_article_page_content(md_content)[1]
-            create_article_page(article_template, pahtway, article_content)
+            article_content, pahtway = get_article_content(md_content,
+                                                           ARTICLE_CSS_PATHWAY)
+            create_article_page(article_template, article_content, pahtway)
